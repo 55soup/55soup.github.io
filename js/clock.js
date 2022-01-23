bc681c2f2ae8667991069c15bb73ff4a140a52ee
Park Sunjoo <86298664+55soup@users.noreply.github.com>
2022-01-24 07:38:59 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -14,7 +14,10 @@ function getClock(){ ///"1".padEnd(2,"0") => 20
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0"); ///"1".padStart(2,"0") => 01 
     const minutes = String(date.getMinutes()).padStart(2,"0");
-    clock.innerText=`${hours}:${minutes}`;
+    clock.innerText=`${hours}꞉${minutes}`;
+        setTimeout(function() {
+            clock.innerText=`${hours} ${minutes}`;
+          }, 500);
 }
 
 getClock();
