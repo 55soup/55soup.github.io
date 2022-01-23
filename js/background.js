f263eabcdc01883e47d77cac4e31c75e91cc2ae4
Park Sunjoo <86298664+55soup@users.noreply.github.com>
2022-01-24 05:58:41 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -10,6 +10,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+
 document.body.appendChild(bgImage);
 ///appendChild : element를 밑에 삽입
 ///prepend : element를 위에 삽입
